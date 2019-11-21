c92d5d58c88042f40cf26a676864aa2e88149ff8
ferd <fernandohoflinger@gmail.com>
2019-11-21 19:36:18 -0300

    cadastro de  usuario

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootswatch/dist/flatly/bootstrap.css'
 import Login from './views/login'
 import './custom.css'
+import CadastroUsuarios from './views/cadastrousuario';
 
 class App extends React.Component {
 
@@ -10,7 +11,7 @@ class App extends React.Component {
   render() {
   return (
     <div>
-      <Login/>
+      <CadastroUsuarios/>
     </div>
 
   );
---
+++ src/views/cadastrousuario.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Card from '../components/card'
+import FormGroup from '../components/form-group'
+
+class CadastroUsuarios extends React.Component {
+    state = {
+        nome : "",
+        email : "",
+        senha : "",
+        senhaRepeticao : ""
+    }
+
+    cadastrar = () => {
+        console.log(this.state)
+    }
+    render () {
+        return(
+            <div className="container">
+                <Card title="Cadastro de Usuário">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="bs-component">
+                            <FormGroup htmlFor="inputNome" label="Nome: *">
+                                            <input value={this.state.nome} onChange={e => this.setState({nome: e.target.value})} 
+                                            type="text" className="form-control" id="inputNome" placeholder="Digite o nome"/>
+                            </FormGroup>  
+
+                            <FormGroup htmlFor="ImputEmail" label="Email: *">
+                                            <input value={this.state.email} onChange={e => this.setState({email: e.target.value})} 
+                                            type="email" className="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Digite o Email"/>
+                            </FormGroup>  
+                            
+                            <FormGroup htmlFor="exampleImputPassword1" label="Senha: *">
+                                         <input  value={this.state.senha} onChange={e => this.setState({senha: e.target.value})} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                            </FormGroup>
+
+                            <FormGroup htmlFor="exampleImputPassword2" label="Repita a Senha: *">
+                                         <input  value={this.state.senhaRepeticao} onChange={e => this.setState({senhaRepeticao: e.target.value})} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                            </FormGroup>
+                            <button onClick ={this.cadastrar} className="btn btn-success">Salvar</button>
+                            <button className="btn btn-danger">Cancelar</button>
+
+                            </div>
+                        </div>
+                    </div>  
+                </Card>
+            </div>            
+
+        )
+    }
+}
+
+export default CadastroUsuarios
